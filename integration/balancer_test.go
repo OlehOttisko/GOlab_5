506c1ca5ddfadf1b636d92ddfdb9569d8335b8a4
Nikita <115003350+kikoser@users.noreply.github.com>
2024-06-06 20:58:23 +0300

    Update balancer_test.go

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -56,7 +56,7 @@ func (s *IntegrationSuite) TestBalancer(c *C) {
 			responseSize = 2000
 		}
 		server, _ := sendRequest(baseAddress, responseSize, &client)
-		c.Assert(server.Header.Get("lb-from"), Equals, fmt.Sprintf("server%d:8080", "server[%d+1]:8080"))
+		c.Assert(server.Header.Get("lb-from"), Equals, fmt.Sprintf("server%d:8080", serverNum[i]+1))
 
 	}
 }
